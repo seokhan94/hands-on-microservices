0a7a44b417a168217d06b9968d73648457540e5b
seokhan <seokhan94@naver.com>
2022-12-12 01:42:13 +0900

    fix: 전체 build 오류 수정

--- util/src/main/java/se/magnus/util/http/GlobalControllerExceptionHandler.java
+++ util/src/main/java/se/magnus/util/http/GlobalControllerExceptionHandler.java
@@ -1,6 +1,7 @@
 package se.magnus.util.http;
 
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,8 +12,8 @@ import se.magnus.util.exceptions.InvalidInputException;
 import se.magnus.util.exceptions.NotFoundException;
 
 @ControllerAdvice
-@Slf4j
 public class GlobalControllerExceptionHandler {
+    private static final Logger log = LoggerFactory.getLogger(GlobalControllerExceptionHandler.class);
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(NotFoundException.class)
